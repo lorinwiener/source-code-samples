411d4ed44afbb2087524571e5099c88f39c44fcf
lorinwiener <lorin@lorinwiener.com>
2018-07-30 15:50:53 -0700

    Added constants

---
+++ reactJS - ES6/constants/constants.js
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2016 AT&T, Inc.
+ *
+ * NGC Constants
+ *
+ * @version: 1.0
+ */
+
+//Data
+export const FETCH_CHANNEL_ISLANDS = "FETCH_CHANNEL_ISLANDS";
+export const SELECT_CHANNEL_ISLAND = "SELECT_CHANNEL_ISLAND";
+export const FETCH_SHOWS = "FETCH_SHOWS";
+export const FETCH_VIDEOS = "FETCH_VIDEOS";
+
+// Global
+export const VOID_ACTION = "VOID_ACTION";
+export const TOGGLE_MENU = "TOGGLE_MENU";
+
+export const SOUND_ACTION = "SOUND_ACTION";
+
+// Common Info Page
+export const LOAD_COMMON_INFO_PAGE = "LOAD_COMMON_INFO_PAGE";
+
+// Record/Cancel
+export const SCHEDULE_RECORDING = "SCHEDULE_RECORDING";
+export const SCHEDULE_SERIES_RECORDING = "SCHEDULE_SERIES_RECORDING";
+export const UPDATE_RECORDING = "UPDATE_RECORDING";
+export const CANCEL_RECORDING = "CANCEL_RECORDING";
+export const CANCEL_SERIES_RECORDING = "CANCEL_SERIES_RECORDING";
+export const CANCEL_SERIES_REQ = 'CANCEL_SERIES_REQ';
+export const CANCEL_SERIES_ACK = 'CANCEL_SERIES_ACK';
+export const TOGGLE_RECORD_OPTIONS = "TOGGLE_RECORD_OPTIONS";
+export const TOGGLE_COMMON_INFO_PAGE = "TOGGLE_COMMON_INFO_PAGE";
+export const TOGGLE_RECORDING_MSG = "TOGGLE_RECORDING_MSG";
+
+// Trick Playing
+export const UPDATE_PRIMARY_VIDEO_TIMESTAMP = "UPDATE_PRIMARY_VIDEO_TIMESTAMP";
+export const UPDATE_PRIMARY_VIDEO_STATE = "UPDATE_PRIMARY_VIDEO_STATE";
+export const UPDATE_SECONDARY_VIDEO_TIMESTAMP = "UPDATE_SECONDARY_VIDEO_TIMESTAMP";
+export const UPDATE_SECONDARY_VIDEO_STATE = "UPDATE_SECONDARY_VIDEO_STATE";
+export const CLEAR_TRICKPLAY = "CLEAR_TRICKPLAY";
+
+// Navigation
+export const UP = "UP";
+export const DOWN = "DOWN";
+export const LEFT = "LEFT";
+export const RIGHT = "RIGHT";
+export const MIDDLE = 'MIDDLE';
+export const UNKNOWN = 'UNKNOWN';
+export const SET_PAGE_MATRIX = "SET_PAGE_MATRIX";
+
+// Playlist
+export const BUILD_PLAYLIST_MODEL = "BUILD_PLAYLIST";
+export const TOGGLE_PLAYLIST = "TOGGLE_PLAYLIST";
+export const UPDATE_PLAYLIST_MODEL = "UPDATE_PLAYLIST";
+export const UPDATE_PLAYLIST_STATE = "UPDATE_PLAYLIST_STATE";
+
+export const HIGHLIGHT_FULL_SCREEN_AV_OPTION = 'HIGHLIGHT_FULL_SCREEN_AV_OPTION';
+export const SELECT_FULL_SCREEN_AV_OPTION = 'SELECT_FULL_SCREEN_AV_OPTION';
+
+//Notify
+export const NOTIFY_CAROUSELS_READY = 'NOTIFY_CAROUSELS_READY';
+export const NOTIFY_CAROUSEL_ITEMS_READY = 'NOTIFY_CAROUSEL_ITEMS_READY';
+export const UPDATE_TAB_INDEX = 'UPDATE_TAB_INDEX';
+export const UPDATE_CAROUSEL_INDEX = 'UPDATE_CAROUSEL_INDEX';
+export const UPDATE_NUM_OF_CAROUSELS = 'UPDATE_NUM_OF_CAROUSELS';
+export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
+
+//NGC Data
+export const FETCH_MENU = "FETCH_MENU";
+export const FETCH_ICONS = "FETCH_ICONS";
+
+//Contextual
+export const NUMBER_OF_CABS = "NUMBER_OF_CABS";
+export const ENABLE_CAM = "ENABLE_CAM";
+export const DISABLE_CAM = "DISABLE_CAM";
+
+export const TOGGLE_TOUCH_VIEW = "TOGGLE_TOUCH_VIEW";
+export const TOUCH = "TOUCH";
+
+export const TAP = "TAP";
+export const CLICK = "CLICK";
+export const SWIPE = "SWIPE";
+export const DRAG = "DRAG";
+export const KEYBOARD = "KEYBOARD";
+export const T9 = "T9";
+
+export const MIC = "MIC";
+// export const T9 = "T9";
+export const UPDATE_KEYWORD = "UPDATE_KEYWORD";
+
+export const ON = "ON";
+export const OFF = "OFF";
+
+export const Settings = 'Settings';
+
+export const USER_FRIENDLY_NAME = 'USER_FRIENDLY_NAME';
+
+export const REC = 'REC';
+
+export const PLAY = 'PLAY';
+export const PAUSE = 'PAUSE';
+export const FFW = 'FFW';
+export const REW = 'REW';
+export const JUMP = 'JUMP';
+
+export const NOOP = 'NOOP';
